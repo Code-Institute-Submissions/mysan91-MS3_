2f836a0d1b282e6c8a8644125bbfa2a4c2551f6f
mysan91 <mysans1@live.se>
2021-04-10 20:06:28 +0000

    add tasks.html

--- env.py
+++ env.py
@@ -4,5 +4,5 @@ os.environ.setdefault("IP", "0.0.0.0")
 os.environ.setdefault("PORT", "5000")
 os.environ.setdefault("SECRET_KEY", "QlRJzdRRG1")
 os.environ.setdefault(
-    "MONGO_URI", "mongodb+srv://milestone3:<milestone3>@milestone.3exkg.mongodb.net/Task_manager?retryWrites=true&w=majority")
+    "MONGO_URI", "mongodb+srv://milestone3:Porsche997@milestone.3exkg.mongodb.net/Task_manager?retryWrites=true&w=majority")
 os.environ.setdefault("MONGO_DBNAME", "task_manager")
